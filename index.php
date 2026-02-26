a6f0def2e4bc954a37502ec3d778ee312d5c7b9f
Sam Obila Allela <Obilasam3@gmail.com>
2026-02-26 11:51:50 +0300

    Fixing file routing

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 // Remove leading slash
